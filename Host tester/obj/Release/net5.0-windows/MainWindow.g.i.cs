68a072d6f245e9cb910bb8cf06b5b2973b33f9ba
Nati892 <netanel892@gmail.com>
2022-06-14 11:54:07 +0300

    deleted /debug /release

--- Host tester/obj/Release/net5.0-windows/MainWindow.g.i.cs
+++ Host tester/obj/Release/net5.0-windows/MainWindow.g.i.cs
@@ -116,7 +116,7 @@ namespace Host_tester {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Host tester;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Host tester;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
